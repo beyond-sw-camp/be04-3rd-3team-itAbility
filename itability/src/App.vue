7b58165337a7bf83b5241334d076853dfb2ef0a0
daersh <daersh@naver.com>
2024-03-27 09:37:01 +0900

    :memo: [Add] Vue 환경 구축

---
+++ itability/src/App.vue
@@ -0,0 +1,21 @@
+
+<script setup>
+  import MyPageMain from "./components/mypage/MyPageMain.vue";
+  import Header from "./components/Header.vue";
+  import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <Header/>
+  
+  <div class="mypage"><MyPageMain/></div>
+
+  <Footer/>
+</template>
+
+<style scoped>
+  .mypage{
+    padding: 0 20%;
+    padding-top: 20px;
+  }
+</style>
